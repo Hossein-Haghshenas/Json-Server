5ebe4199e2231022e6fafd1617e6b3633ffa03ea
Hossein <haghshenas3c@gmail.com>
2022-04-05 19:41:35 +0430

    get data from json file and show in dom

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // javascript for index.html
+
+const renderPosts = async () => {
+  const url = "http://localhost:3000/posts";
+
+  const res = await fetch(url);
+  const data = await res.json();
+  displayData(data);
+};
+renderPosts();
+
+const displayData = (data) => {
+  for (const items of data) {
+    const container = document.createElement("section");
+    container.classList.add("post");
+    const title = document.createElement("h2");
+    title.textContent = items.title;
+    const like = document.createElement("p");
+    like.textContent = `${items.likes} likes`;
+    const article = document.createElement("p");
+    article.textContent = items.body;
+    const details = document.createElement("a");
+    details.href = "./details.html";
+    details.textContent = "Read more ...";
+    container.append(title, like, article, details);
+
+    document.querySelector(".blogs").appendChild(container);
+  }
+};
